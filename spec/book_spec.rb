e36558dd6855e154010af5838bfd05ec01822476
Beverly Lau <beverly.a.lau@gmail.com>
2016-01-28 12:56:21 -0800

    Use new syntax for assertion, add library specs and Library class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,23 +8,23 @@ describe Book do
 
   describe '#new' do
     it 'returns a new book object' do
-      @book.should be_an_instance_of Book
+      expect(@book).to be_an_instance_of Book
     end
 
     it 'throws ArgumentError when given fewer than 3 params' do
-      lambda { Book.new 'Title', 'Author' }.should raise_exception ArgumentError
+      expect(lambda { Book.new 'Title', 'Author' }).to raise_exception ArgumentError
     end
   end
 
   describe '#title' do
     it 'returns the correct title' do
-      @book.title.should eql 'Title'
+      expect(@book.title).to eql 'Title'
     end
     it 'returns the correct author' do
-      @book.author.should == 'Author'
+      expect(@book.author).to eql 'Author'
     end
     it 'returns the correct category' do
-      @book.category.should eql :category
+      expect(@book.category).to eql :category
     end
   end
 
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,1 +1,37 @@
 require 'spec_helper'
+
+describe 'Library Object' do
+
+  before :all do
+    lib_arr = [
+        Book.new('JavaScript: The Good Parts', 'Douglas Crockford', :development),
+        Book.new('Designing with Web Standards', 'Jeffrey Zeldman', :design),
+        Book.new('Don\'t Make Me Think', 'Steve Krug', :usability),
+        Book.new('JavaScript Patterns', 'Stoyan Stefanov', :development),
+        Book.new('Responsive Web Design', 'Ethan Marcotte', :design)
+    ]
+    File.open 'books.yml', 'w' do |f|
+      f.write YAML::dump lib_arr
+    end
+  end
+
+  before :each do
+    @lib = Library.new 'books.yml'
+  end
+
+  describe '#new' do
+    context 'with no parameters' do
+      it 'has no books' do
+        lib = Library.new
+        expect(lib.books.length).to eq(0)
+      end
+    end
+
+    context 'with a yaml file name parameter' do
+      it 'has five books' do
+        expect(@lib.books.length).to eq(5)
+      end
+    end
+  end
+
+end
